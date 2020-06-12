628449caa359b95dae99bc9f05abbc4294d5cae7
Evelyn Chee <evelyn.chee.2017@sis.smu.edu.sg>
2020-06-12 14:10:41 +0800

    Add favouriteFruits ArrayList, import Gson and add function to convert to Json using Gson

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,8 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import java.util.*;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,9 +26,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Evelyn!</h1>");
-  }
+    ArrayList<String> favouriteFruits = new ArrayList<>(Arrays. asList("Mango", "Strawberry", "Grapes"));
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       
+        //converts the arrayList to Json using convertToJsonUsingGson
+        String json = convertToJsonUsingGson(favouriteFruits);
+
+        // Send the JSON as the response
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+    }
+
+    private String convertToJsonUsingGson(ArrayList<String> list) {
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return json;
+    }
+
 }
