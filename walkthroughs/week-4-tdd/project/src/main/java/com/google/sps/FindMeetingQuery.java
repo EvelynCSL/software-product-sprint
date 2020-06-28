567f089d89a19991babeb51a4f53b0541b87d1d0
Evelyn Chee <evelyn.chee.2017@sis.smu.edu.sg>
2020-06-28 20:24:04 +0800

    Implement query()

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,79 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+    final long MINUTES_IN_A_DAY = 1440;
+    final int START_OF_DAY = TimeRange.START_OF_DAY;
+    final int END_OF_DAY = TimeRange.END_OF_DAY;
+    int endOfTimeRange = 0;
+    TimeRange previousTimeRange = TimeRange.fromStartEnd(0,0,true);
+    Collection<TimeRange> unavailableTimeRange = new ArrayList<>();
+    Collection<TimeRange> unavailableNoOverlap = new ArrayList<>();
+    Collection<TimeRange> returnCollection = new ArrayList<>();
+
+    long durationOfRequest = request.getDuration();
+    Collection<String> attendeesOfRequest = request.getAttendees();
+
+    if(attendeesOfRequest.isEmpty()){
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }else if(durationOfRequest > MINUTES_IN_A_DAY){
+      return Arrays.asList();
+    }else{
+      for (String attendee : attendeesOfRequest){
+        System.out.println(attendee);
+        for (Event event : events){
+          if(event.getAttendees().contains(attendee)){
+            System.out.println(event.getWhen());
+            unavailableTimeRange.add(event.getWhen());
+          }
+        }
+      }
+      System.out.println(unavailableTimeRange);
+      // Collections.sort(unavailableTimeRange, ORDER_BY_START);
+
+      for (TimeRange timerange : unavailableTimeRange){
+        if(previousTimeRange.contains(timerange)){
+        }else if(previousTimeRange.overlaps(timerange)){
+          TimeRange newTimeRange = TimeRange.fromStartEnd(previousTimeRange.start(),timerange.end(),false);
+          unavailableNoOverlap.add(newTimeRange);
+          unavailableNoOverlap.remove(previousTimeRange);
+          previousTimeRange = newTimeRange;
+        }else{
+          unavailableNoOverlap.add(timerange);
+          previousTimeRange = timerange;
+        }
+      }
+      System.out.println(unavailableTimeRange);
+      System.out.println(unavailableNoOverlap);
+
+      for (TimeRange timerange : unavailableNoOverlap){
+        if(endOfTimeRange == 0){
+          TimeRange trval = TimeRange.fromStartEnd(START_OF_DAY,timerange.start(),false);
+          if(trval.duration() != 0 && trval.duration() >= durationOfRequest){
+            returnCollection.add(trval);
+          }
+        }else{
+          TimeRange trval = TimeRange.fromStartEnd(endOfTimeRange,timerange.start(),false);
+          if(trval.duration() != 0 && trval.duration() >= durationOfRequest){
+            returnCollection.add(trval);
+          }
+        }
+        endOfTimeRange = timerange.end();
+      }
+      TimeRange trval = TimeRange.fromStartEnd(endOfTimeRange,END_OF_DAY,true);
+      if(trval.duration() != 0 && trval.duration() >= durationOfRequest){
+        returnCollection.add(trval);
+      }
+    }
+
+    return returnCollection;
   }
 }
